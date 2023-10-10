bfa1a47c855f68f54fb9ceec35d6c1bfa0fb7b8f
immanuel-savvy <immanuelsavvy@gmail.com>
2023-10-10 04:43:26 +0100

    1696909406042768922

--- src/Screens/Withdraw.js
+++ src/Screens/Withdraw.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {TextInput, View} from 'react-native';
+import {TextInput, View, KeyboardAvoidingView, ScrollView} from 'react-native';
 import {emitter} from '../../Udara';
 import {hp, wp} from '../utils/dimensions';
 import {get_request, post_request} from '../utils/services';
@@ -126,113 +126,120 @@ class Withdraw extends React.Component {
     return (
       <Bg_view flex>
         <Header title="Withdraw" navigation={navigation} />
-        <Bg_view flex style={{justifyContent: 'center'}}>
-          <Bg_view
-            style={{
-              elevation: 10,
-              margin: wp(5.6),
-              padding: wp(2.8),
-              shadowColor: '#000',
-              borderRadius: wp(4),
-            }}>
-            <Fr_text bold size={wp(5)} style={{margin: wp(2.8)}}>
-              {paycheck ? `Paycheck` : `Amount to withdraw`}
-            </Fr_text>
-            <Bg_view
-              horizontal
-              style={{
-                alignItems: 'center',
-                borderRadius: wp(1.4),
-                shadowColor: '#000',
-                elevation: 5,
-                margin: wp(2.8),
-              }}>
-              <TextInput
-                placeholderTextColor="#ccc"
-                placeholder="Enter amount"
-                autoFocus
-                value={value}
-                keyboardType="phone-pad"
-                onChangeText={this.set_value}
+        <ScrollView contentContainerStyle={{justifyContent: 'center', flex: 1}}>
+          <KeyboardAvoidingView style={{flex: 1, justifyContent: 'center'}}>
+            <Bg_view flex style={{justifyContent: 'center'}}>
+              <Bg_view
                 style={{
-                  flex: 1,
-                  borderRadius: wp(1),
+                  elevation: 10,
+                  margin: wp(5.6),
                   padding: wp(2.8),
-                  fontSize: wp(5),
-                  color: '#000',
-                }}
-              />
-              <View>
+                  shadowColor: '#000',
+                  borderRadius: wp(4),
+                }}>
+                <Fr_text bold size={wp(5)} style={{margin: wp(2.8)}}>
+                  {paycheck ? `Paycheck` : `Amount to withdraw`}
+                </Fr_text>
                 <Bg_view
                   horizontal
                   style={{
-                    borderRadius: wp(1),
-                    height: hp(7.5),
-                    padding: wp(2.8),
-                    borderLeftColor: '#ccc',
-                    borderLeftWidth: 1,
+                    alignItems: 'center',
+                    borderRadius: wp(1.4),
+                    shadowColor: '#000',
+                    elevation: 5,
+                    margin: wp(2.8),
                   }}>
-                  <Icon
-                    icon={require('../assets/Icons/nigeria_flag_rectangle.png')}
+                  <TextInput
+                    placeholderTextColor="#ccc"
+                    placeholder="Enter amount"
+                    autoFocus
+                    value={value}
+                    keyboardType="phone-pad"
+                    onChangeText={this.set_value}
+                    style={{
+                      flex: 1,
+                      borderRadius: wp(1),
+                      padding: wp(2.8),
+                      fontSize: wp(5),
+                      color: '#000',
+                    }}
                   />
-                  <Fr_text style={{marginLeft: wp(1.4)}}>{'NGN'}</Fr_text>
+                  <View>
+                    <Bg_view
+                      horizontal
+                      style={{
+                        borderRadius: wp(1),
+                        height: hp(7.5),
+                        padding: wp(2.8),
+                        borderLeftColor: '#ccc',
+                        borderLeftWidth: 1,
+                      }}>
+                      <Icon
+                        icon={require('../assets/Icons/nigeria_flag_rectangle.png')}
+                      />
+                      <Fr_text style={{marginLeft: wp(1.4)}}>{'NGN'}</Fr_text>
+                    </Bg_view>
+                  </View>
                 </Bg_view>
-              </View>
-            </Bg_view>
 
-            {paycheck ? (
-              <Bg_view>
-                {paycheck_account ? (
-                  <Bank_account account={paycheck_account} action={() => {}} />
+                {paycheck ? (
+                  <Bg_view>
+                    {paycheck_account ? (
+                      <Bank_account
+                        account={paycheck_account}
+                        action={() => {}}
+                      />
+                    ) : (
+                      <Loadindicator />
+                    )}
+                  </Bg_view>
+                ) : bank_account ? (
+                  <Bank_account
+                    account={bank_account}
+                    action={() => this.bank_accounts?.toggle_show_modal()}
+                  />
                 ) : (
-                  <Loadindicator />
+                  <Bg_view style={{alignItems: 'center'}}>
+                    <Text_btn
+                      text="Select Bank Account"
+                      centralise
+                      accent
+                      action={() => this.bank_accounts?.toggle_show_modal()}
+                    />
+                  </Bg_view>
                 )}
-              </Bg_view>
-            ) : bank_account ? (
-              <Bank_account
-                account={bank_account}
-                action={() => this.bank_accounts?.toggle_show_modal()}
-              />
-            ) : (
-              <Bg_view style={{alignItems: 'center'}}>
-                <Text_btn
-                  text="Select Bank Account"
-                  centralise
-                  accent
-                  action={() => this.bank_accounts?.toggle_show_modal()}
+
+                {message ? (
+                  <Text_btn
+                    italic
+                    centralise
+                    text={
+                      perhaps
+                        ? "It could be that your topup hasn't been fully processed. This usually take around 30 mins. T:messagery again."
+                        : message
+                    }
+                  />
+                ) : null}
+
+                <Stretched_button
+                  disabled={disabled}
+                  loading={loading}
+                  title="continue"
+                  action={this.withdraw}
                 />
+
+                <Cool_modal
+                  ref={bank_accounts => (this.bank_accounts = bank_accounts)}>
+                  <Bank_accounts
+                    user={user}
+                    action={this.set_bank_account}
+                    toggle={() => this.bank_accounts?.toggle_show_modal()}
+                  />
+                </Cool_modal>
               </Bg_view>
-            )}
-
-            {message ? (
-              <Text_btn
-                italic
-                centralise
-                text={
-                  perhaps
-                    ? "It could be that your topup hasn't been fully processed. This usually take around 30 mins. T:messagery again."
-                    : message
-                }
-              />
-            ) : null}
-
-            <Stretched_button
-              disabled={disabled}
-              loading={loading}
-              title="continue"
-              action={this.withdraw}
-            />
-
-            <Cool_modal
-              ref={bank_accounts => (this.bank_accounts = bank_accounts)}>
-              <Bank_accounts
-                user={user}
-                action={this.set_bank_account}
-                toggle={() => this.bank_accounts?.toggle_show_modal()}
-              />
-            </Cool_modal>
-          </Bg_view>
-        </Bg_view>
+            </Bg_view>
+          </KeyboardAvoidingView>
+        </ScrollView>
       </Bg_view>
     );
   };
